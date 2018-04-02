e89415900a90f51b74923de46041ebf85de8ec5d
BuildTools <unconfigured@null.spigotmc.org>
2018-04-02 08:46:33 +0200

    map aggiustate

--- Front End/SitoFinale/assets/js/locationMap.js
+++ Front End/SitoFinale/assets/js/locationMap.js
@@ -1,4 +1,6 @@
-(function initGoogleMaps()
+var map;
+
+function initGoogleMaps()
 {
     var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
     var mapOptions = {
@@ -7,7 +9,7 @@
         scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
         styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
     }
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
     var marker = new google.maps.Marker({
         position: myLatlng,
@@ -16,4 +18,57 @@
 
     // To add the marker to the map, call setMap();
     marker.setMap(map);
-})();
+
+  /*  var flightPlanCoordinates = [
+    {lat: 37.772, lng: -122.214},
+    {lat: 21.291, lng: -157.821},
+    {lat: -18.142, lng: 178.431},
+    {lat: -27.467, lng: 153.027}
+  ];
+  var flightPath = new google.maps.Polyline({
+    path: flightPlanCoordinates,
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+  });
+
+  flightPath.setMap(map);*/
+  var trafficLayer = new google.maps.TrafficLayer();
+        trafficLayer.setMap(map);
+};
+
+function AddCoordinate(x1,y1){ addCoords(x1,y1, map); }
+
+function addCoords(x1,y1, mapPointer)
+{
+  var myLat = new google.maps.LatLng(x1,y1);
+  var marker = new google.maps.Marker({
+      position: myLat,
+      title:"Posizione registrata"
+  });
+
+  marker.setMap(mapPointer);
+}
+
+function addPath(var flightPlanCoordinates)
+{
+  //ESEMPIO DI flightPlanCoordinates
+    /*var flightPlanCoordinates = [
+    {lat: 37.772, lng: -122.214},
+    {lat: 21.291, lng: -157.821},
+    {lat: -18.142, lng: 178.431},
+    {lat: -27.467, lng: 153.027}
+  ];*/
+  //RICORDA PER AGGIUNGERE flightPlanCoordinates.Push({lat: valoreVariabile1, lng: valoreVariabile2})
+  var flightPath = new google.maps.Polyline({
+    path: flightPlanCoordinates,
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+  });
+
+  flightPath.setMap(map);
+
+}
